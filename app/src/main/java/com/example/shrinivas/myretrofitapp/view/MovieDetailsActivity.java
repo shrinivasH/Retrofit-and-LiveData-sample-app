842ecd8b431a50f2a12a6b80cb9c6976ec23c3f5
shrinivasH <sholgundikar@gmail.com>
2017-11-27 13:23:09 +0530

    UI changes in Movie setails screen is done.

--- app/src/main/java/com/example/shrinivas/myretrofitapp/view/MovieDetailsActivity.java
+++ app/src/main/java/com/example/shrinivas/myretrofitapp/view/MovieDetailsActivity.java
@@ -9,7 +9,7 @@ import com.example.shrinivas.myretrofitapp.model.dto.LocalMovie;
 
 public class MovieDetailsActivity extends AppCompatActivity {
 
-    private TextView mMovieName, mMovieDetails;
+    private TextView mMovieName, mMovieDetails, mMovieRatings;
     private LocalMovie localMovie;
 
     @Override
@@ -18,11 +18,13 @@ public class MovieDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_details);
         mMovieName = (TextView) findViewById(R.id.MovieName);
         mMovieDetails = (TextView) findViewById(R.id.MovieDescription);
+        mMovieRatings = (TextView) findViewById(R.id.MovieRating);
         Bundle getData = getIntent().getExtras();
         if (getData != null) {
             localMovie = getData.getParcelable("Movie");
         }
         mMovieName.setText(localMovie.getOriginal_title());
         mMovieDetails.setText(localMovie.getOverview());
+        mMovieRatings.setText(""+localMovie.getPopularity());
     }
 }
